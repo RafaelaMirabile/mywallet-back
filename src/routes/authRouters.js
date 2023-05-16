ab606ed534b5543688ab15e399c9a3afda8be717
RafaelaMirabile <rafaelamirabile@gmail.com>
2023-05-16 12:56:35 -0300

    chore: 5 attempt to deploy

--- src/routes/authRouters.js
+++ src/routes/authRouters.js
@@ -4,6 +4,9 @@ import { login, signUp } from "../controllers/authController.js";
 const authRouter = Router();
 
 authRouter
+.get("/", (req, res) => {
+    res.send("Express on Vercel");
+  })
 .post('/login',login)
 .post('/signup',signUp);
 
