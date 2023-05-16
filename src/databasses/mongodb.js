58b653b752b130b6d348b74da3cb4f6c88cf471e
RafaelaMirabile <rafaelamirabile@gmail.com>
2023-05-16 13:59:34 -0300

    chore: attempt to deploy- move .env file to src

--- src/databasses/mongodb.js
+++ src/databasses/mongodb.js
@@ -4,7 +4,7 @@ import { MongoClient } from 'mongodb';
 dotenv.config();
 let db = null;
 console.log(process.env.MONGO_ATLAS_URI);
-const mongoClient = new MongoClient (process.env.MONGO_ATLAS_URI);
+const mongoClient = new MongoClient(process.env.MONGO_ATLAS_URI);
 try{
     await mongoClient.connect();
     db = mongoClient.db('mywallet');
