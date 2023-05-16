760b4799bfa1cbd70125b92b160d7265d2727133
RafaelaMirabile <rafaelamirabile@gmail.com>
2023-05-16 13:18:00 -0300

    chore: attempt to deploy w cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import authRouter from './src/routes/authRouters.js'
 import transactionsRouter from './src/routes/transactionRoutes.js';
 
 const server = express();
 server.use(express.json());
+server.use('*',cors());
 server.use(authRouter);
 server.use(transactionsRouter);
+
 server.listen(5000,()=> console.log('listening on port 5000'));
 
 module.exports = server;
