79c737f15d0fcbb42494b1020d0a997bd08b0476
RafaelaMirabile <rafaelamirabile@gmail.com>
2023-05-18 11:20:30 -0300

    feat: database_uri for rnning project on localhost

--- src/databasses/mongodb.js
+++ src/databasses/mongodb.js
@@ -4,7 +4,7 @@ import { MongoClient } from 'mongodb';
 dotenv.config();
 let db = null;
 
-const mongoClient = new MongoClient(process.env.MONGO_ATLAS_URI);
+const mongoClient = new MongoClient(process.env.DATABASE_URI);
 try{
     await mongoClient.connect();
     db = mongoClient.db('mywallet');
